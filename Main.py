3479c903e3d1760d95f5cde3b16a5f3afb483f1f
johan <johanerikpukk@gmail.com>
2019-09-26 02:43:09 +0300

    fixed typos in comments and refactored function "counter()" to "increment()"

--- Main.py
+++ Main.py
@@ -9,17 +9,14 @@ except FileNotFoundError:
     print("Please add the token to the file token.txt in this directory")
     exit()
 
-client = Bot('notneeded')  # 'notneeded' is the bot command prefix, as this bot has no commands the prefix is not needed
-# and that is reflected in the name
+client = Bot('notneeded')  # 'notneeded' is the bot's command prefix (for example in !help "!" would be the command
+# prefix), as this bot has no commands the prefix is not needed and that is reflected in the name
 
-lastMessage = 0  # start counting at 0, so the first message should be 1, will egt overwritten with the last message
-# if it was numerical
-
-
-# to the channel, that would enable restarting the bot while a count is going
+lastMessage = 0  # start counting at 0, so the first message should be 1, will get overwritten with the last message of
+# the channel if it was numerical
 
 
-def counter():  # count up by 1
+def increment():  # count up by 1
     global lastMessage
     lastMessage += 1
 
@@ -29,7 +26,7 @@ def reset():  # reset the counter
     lastMessage = 0
 
 
-async def get_last_message():  # returns the last message if it was numberical, 0 otherwise
+async def get_last_message():  # returns the last message if it was numerical, 0 otherwise
     for server in client.guilds:
         if server.name == "Bot testing":
             for channel in server.channels:
@@ -60,14 +57,14 @@ async def on_ready():  # set the "playing:" message and print when the bot is lo
 async def on_message(message):
     if str(message.channel) == 'counting':  # check the message is in the right channel
         if message.content == (str(lastMessage + 1)):  # check the message is correct
-            counter()  # increment the counter
+            increment()  # increment the counter
         else:
             print("reset", message.content)  # print the message that reset the counter
             reset()  # reset the counter to 0
             member = message.author  # get the user who sent the message
             role = get(member.guild.roles, name="Failed")  # get the id of the role with the name "Failed"
             await member.add_roles(role)  # add the role to the user
-            await message.add_reaction('\U0000274C')  # react with a X
+            await message.add_reaction('\U0000274C')  # react with an X
     await client.process_commands(message)
 
 
